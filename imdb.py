d9fc4996473d43deca3c81cf4da6258c3c99f1cf
Herve07h22 <contact@storeed.me>
2022-02-15 22:52:38 +0100

    first commit

---
+++ imdb.py
@@ -0,0 +1,34 @@
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+import numpy as np
+import torch
+
+raw_datasets = load_dataset("imdb")
+metric = load_metric("accuracy")
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(100))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(100))
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model.to(device)
+
+training_args = TrainingArguments("test_trainer")
+
+trainer = Trainer(model=model, args=training_args, train_dataset=small_train_dataset, eval_dataset=small_eval_dataset, compute_metrics=compute_metrics)
+
+trainer.train()
+
+trainer.evaluate()
